4c2fd2c3dd3599f6f80c5c5571bae7078576af9f
ipeknazerel <75453633+ipeknazerel@users.noreply.github.com>
2021-02-16 22:45:26 +0000

    Added the credit URLs

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ import Weather from "./Weather.js";
 function App() {
   return (
     <div className="App">
-      <Weather/>
+      <Weather />
+
+
+      <footer>
+        This project is coded by {" "}
+        <a href="https://www.linkedin.com/in/ipeknaz-erel-49516398/?originalSubdomain=uk" target="_blank"> Ipeknaz Erel</a>
+      and the open source can be found on
+        <a href="https://github.com/ipeknazerel/react-weather-app2" target="_blank"> Github </a>
+      </footer>
     </div>
+
   );
 }
 
